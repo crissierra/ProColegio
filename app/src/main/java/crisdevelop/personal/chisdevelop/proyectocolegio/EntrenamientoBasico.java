c6e4f07ee2dac49f12c96461968a7d526c765bd9
Cristian Sierra <cristian.k.sierra@gmail.com>
2019-03-17 04:00:04 -0500

    17/03/2019  Arregle el problemas del cambio del check añ button. Cree la pantalla de integrantes. Falta seguir con la parte de entrenamiento avanzado. Falta implementar los sonidos

--- app/src/main/java/crisdevelop/personal/chisdevelop/proyectocolegio/EntrenamientoBasico.java
+++ app/src/main/java/crisdevelop/personal/chisdevelop/proyectocolegio/EntrenamientoBasico.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,7 +16,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class EntrenamientoBasico extends AppCompatActivity {
+public class EntrenamientoBasico extends AppCompatActivity  implements View.OnClickListener{
 
         public static int RESULT_SAVE = 1;
         public static int RESULT_CANCEL = 2;
@@ -24,12 +25,35 @@ public class EntrenamientoBasico extends AppCompatActivity {
         private int pontos;
         private EditText edtNome;
 
+    static Button BOTONDO,BOTONRE,BOTONMI,BOTONFA, BOTONSOL, BOTONLA, BOTONSI;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_entrenamiento_basico);
             setPontos(0);
             edtNome = findViewById(R.id.edtNome);
+
+            BOTONDO= findViewById(R.id.botondo);
+            BOTONDO.setOnClickListener(this);
+
+            BOTONRE= findViewById(R.id.botonre);
+            BOTONRE.setOnClickListener(this);
+
+            BOTONMI= findViewById(R.id.botonmi);
+            BOTONMI.setOnClickListener(this);
+
+            BOTONFA= findViewById(R.id.botonfa);
+            BOTONFA.setOnClickListener(this);
+
+            BOTONSOL= findViewById(R.id.botonsol);
+            BOTONSOL.setOnClickListener(this);
+
+            BOTONLA= findViewById(R.id.botonla);
+            BOTONLA.setOnClickListener(this);
+
+            BOTONSI= findViewById(R.id.botonsi);
+            BOTONSI.setOnClickListener(this);
         }
 
         public void tocarSom(View view) {
@@ -51,9 +75,84 @@ public class EntrenamientoBasico extends AppCompatActivity {
             atualSom.getAudio().start();
         }
 
-        public void onCheckBoxClicked(View view) {
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
 
-            boolean checked = ((CheckBox) view).isChecked();
+        {
+            case R.id.botondo:
+
+                if (atualSom.getTitle().contains("DO")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.botonre:
+
+                if (atualSom.getTitle().contains("RE")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.botonmi:
+
+                if (atualSom.getTitle().contains("MI")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.botonfa:
+
+                if (atualSom.getTitle().contains("FA")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.botonsol:
+
+                if (atualSom.getTitle().contains("SOL")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.botonla:
+
+                if (atualSom.getTitle().contains("LA")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.botonsi:
+
+                if (atualSom.getTitle().contains("SI")) {
+                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                    setPontos(getPontos() + 1);
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+    }
+
+
+      public void onCheckBoxClicked(View view) {
+
+            /*boolean checked = ((CheckBox) view).isPressed();
             TextView textView = (TextView) findViewById((R.id._txtViewPontos));
 
             if (view.getId() == R.id._btnRadioMiAgudo) {
@@ -126,7 +225,7 @@ public class EntrenamientoBasico extends AppCompatActivity {
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
                 }
                 ((CheckBox) view).setChecked(false);
-            }
+            }*/
         }
 
         public void salvarJogo(View view) {
